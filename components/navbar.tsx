5ce6665f140ac1c67a83b76a8b101072e8544e0a
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-12-30 21:43:56 -0300

    Added NavBar with Drawer responsive to mobile device

---
+++ components/navbar.tsx
@@ -0,0 +1,107 @@
+import ThemeController from "./theme-controller";
+import Image from "next/image";
+import Link from "next/link";
+
+const BellaLogo = () => (
+  <Link href="/">
+    <Image
+      src="/assets/images/bella.svg"
+      alt="bella"
+      priority={true}
+      width={50}
+      height={50}
+      className="hover:scale-110"
+    />
+  </Link>
+);
+
+const MenuContentMobile = () => (
+  <label
+    htmlFor="my-drawer"
+    tabIndex={0}
+    role="button"
+    className="btn btn-ghost lg:hidden"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-5 w-5"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M4 6h16M4 12h8m-8 6h16"
+      />
+    </svg>
+  </label>
+);
+
+const CenterContentDesktop = () => (
+  <ul className="menu menu-horizontal bg-base-200 rounded-box">
+    <li>
+      <Link href="/">Home</Link>
+    </li>
+    <li>
+      <details>
+        <summary>Blog</summary>
+        <ul className="p-2">
+          <li>
+            <Link href="/blog">General</Link>
+          </li>
+          <li>
+            <Link href="/blog/competitive-programming">
+              Competitive Programming
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog/cybersecurity">Cybersecurity</Link>
+          </li>
+        </ul>
+      </details>
+    </li>
+    <li>
+      <Link href="/about">About me</Link>
+    </li>
+  </ul>
+);
+
+const ContactButton = () => (
+  <button className="btn btn-primary rounded-3xl">
+    <Link href="/contact">Contact</Link>
+  </button>
+);
+
+export default function Navbar() {
+  return (
+    <div className="navbar fixed bg-base-100 shadow-2xl p-3 z-[1000]">
+      <div className="navbar-start">
+        <div className="hidden lg:flex">
+          <BellaLogo />
+        </div>
+        <div className="flex lg:hidden">
+          <MenuContentMobile />
+        </div>
+      </div>
+      <div className="navbar-center">
+        <div className="hidden lg:flex">
+          <CenterContentDesktop />
+        </div>
+        <div className="flex lg:hidden">
+          <ContactButton />
+        </div>
+      </div>
+      <div className="navbar-end">
+        <ThemeController />
+        <div className="hidden lg:flex ml-4">
+          <ContactButton />
+        </div>
+        <div className="flex lg:hidden ml-4">
+          <BellaLogo />
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,48 @@
 import "@/styles/globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { AppProps } from "next/app";
+import Navbar from "@/components/navbar";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <Navbar />
+          <Component {...pageProps} />
+        </div>
+        <div className="drawer-side z-[2500]">
+          <label
+            htmlFor="my-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu p-4 w-8/12 min-h-full bg-base-200 text-base-content">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/blog">Blog</Link>
+              <ul className="p-2">
+                <li>
+                  <Link href="/blog/competitive-programming">
+                    Competitive Programming
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/blog/cybersecurity">Cybersecurity</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link href="/about">About me</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <SpeedInsights />
     </>
   );
