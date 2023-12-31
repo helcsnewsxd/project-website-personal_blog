c7d4c00f35062b8b8bfda9c087b1636b5bb67019
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-12-31 16:53:27 -0300

    Fixed exclamation error

--- components/welcome-banner.tsx
+++ components/welcome-banner.tsx
@@ -9,7 +9,7 @@ export default function WelcomeBanner() {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content mt-10 mb-10">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold text-balance">Bienvenido!</h1>
+          <h1 className="mb-5 text-5xl font-bold text-balance">¡Bienvenido!</h1>
           <p className="mb-5 text-pretty">
             En este sitio podrás encontrar contenido invaluable sobre
             programación competitiva y ciberseguridad: teoría, tips, trucos,
