564dc324ead8bb56a5f737eca8448c3d9c39f709
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-12-31 02:28:08 -0300

    Justify in start all the elements of post card component

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -27,12 +27,12 @@ export default function PostCard({ post }: Props) {
     <div className="card bg-base-100 shadow-xl">
       <div className="card-body">
         <h2 className="card-title text-balance text-xl">{post.title}</h2>
-        <p className="text-pretty font-light text-center text-xs p-1">
+        <p className="text-pretty font-light text-start text-xs p-1">
           {post.author.name}
           <br />
           {dateString}
         </p>
-        <div>
+        <div className="flex flex-row justify-start">
           {limitedTags.map((tag: string) => (
             <span
               key={tag}
@@ -42,7 +42,9 @@ export default function PostCard({ post }: Props) {
             </span>
           ))}
         </div>
-        <p className="text-pretty text-base p-1">{limitedDescription}</p>
+        <p className="text-pretty text-base p-1 text-start">
+          {limitedDescription}
+        </p>
         <div className="card-actions justify-end p-1">
           <Link href={`/blog/posts/${post.slug}`}>
             <button className="btn btn-primary hover:scale-105">Read</button>
