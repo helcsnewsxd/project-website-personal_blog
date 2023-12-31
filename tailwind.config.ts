09f9b97b9fd2f0941133601862822af38dd972f3
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-12-31 03:21:48 -0300

    Tried other color for primary of light theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,16 @@ const config: Config = {
 
   // daisyUI config
   daisyui: {
-    themes: ["light", "night"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "rgb(15, 23, 42)",
+        },
+      },
+      ,
+      "night",
+    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "night", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
