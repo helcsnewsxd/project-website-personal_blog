5e31ca8c9deeb3abea5c0486e85fbee0bd130d1a
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-12-31 19:24:38 -0300

    Hero of blog page with full width

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -63,7 +63,7 @@ export default function Blog({ allPosts }: Props) {
 
   // Component
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen min-w-screen bg-base-200">
       <div className="hero-content text-center mt-20 mb-20">
         <div className="max-w-screen">
           <h1 className="text-5xl text-balance font-bold py-6 mt-10">
