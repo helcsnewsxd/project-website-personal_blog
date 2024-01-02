126d6faff9aaa19b642734cbc55c2a97c27d1e37
helcsnewsxd <cphelcsnewsxd@gmail.com>
2024-01-02 04:47:13 -0300

    Added post structure

--- pages/blog/posts/[slug].tsx
+++ pages/blog/posts/[slug].tsx
@@ -1,19 +1,53 @@
 import type PostType from "../../../interfaces/post";
 import { getPostBySlug, getAllPostsCustom } from "../../../lib/getPosts";
 import markdownToHtml from "../../../lib/markdownToHtml";
+import Image from "next/image";
+import markdownStyles from "@/styles/markdown.module.css";
+import hljs from "highlight.js/lib/core";
+import cpp from "highlight.js/lib/languages/cpp";
+
+hljs.registerLanguage("cpp", cpp);
 
 type Props = {
   post: PostType;
 };
 
 export default function Post({ post }: Props) {
+  let dateString = new Date(post.date).toLocaleString("es-ar", {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+  dateString = dateString[0].toUpperCase() + dateString.slice(1);
+
   return (
-    <div className="pt-32 pl-10 pr-10 pb-10 flex flex-row">
-      {post.title}
-      <br />
-      {post.date}
-      <br />
-      {post.content}
+    <div className="py-32 px-10">
+      <div className="bg-base-100 shadow-2xl rounded-3xl p-10">
+        <p className="mb-10 text-balance text-5xl font-semibold text-center">
+          {post.title}
+        </p>
+        <div className="flex justify-start items-center">
+          <Image
+            src={post.author.picture}
+            alt={post.author.name}
+            className="w-10 h-10 mr-4"
+            width={50}
+            height={50}
+          />
+          <div className="text-lg font-light italic">
+            <p>Redactado por {post.author.name}</p>
+            <p>{dateString}</p>
+          </div>
+        </div>
+
+        <div
+          className={markdownStyles["markdown"]}
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+
+        <div>{post.content}</div>
+      </div>
     </div>
   );
 }
