e4ee7d5544d9ea803170a969d9288a7b3a25bb61
helcsnewsxd <cphelcsnewsxd@gmail.com>
2024-01-02 21:30:19 -0300

    Add link to tag to search in blog search

--- components/posts/individual/badges/post-tag.tsx
+++ components/posts/individual/badges/post-tag.tsx
@@ -1,16 +1,19 @@
+import Link from "next/link";
+
 type Props = {
   tag: string;
   type: Boolean;
 };
 
 export default function PostTagBadge({ tag, type }: Props) {
-  return type ? (
-    <span className="badge badge-outline badge-accent text-pretty text-xs m-1 w-auto h-auto">
+  return (
+    <Link
+      href={`/blog?searchValue=${encodeURIComponent(tag)}`}
+      className={`badge badge-outline badge-accent text-pretty m-1 w-auto h-auto hover:scale-105 ${
+        type ? "text-xs" : "text-sm lg:text-base"
+      }`}
+    >
       {tag}
-    </span>
-  ) : (
-    <span className="badge badge-outline badge-accent text-pretty text-sm lg:text-base m-1 w-auto h-auto">
-      {tag}
-    </span>
+    </Link>
   );
 }
