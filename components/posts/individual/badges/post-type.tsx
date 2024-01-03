fd1983f4247318cc7857d8148f898a5c00ffb608
helcsnewsxd <cphelcsnewsxd@gmail.com>
2024-01-02 21:30:47 -0300

    Used encode URI component in post type badge

--- components/posts/individual/badges/post-type.tsx
+++ components/posts/individual/badges/post-type.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function PostTypeBadge({ type }: Props) {
   return (
     <Link
-      href={`/blog?postType=${type}`}
+      href={`/blog?postType=${encodeURIComponent(type)}`}
       className="badge badge-accent rounded-md text-pretty text-xs m-1 text-base-100 hover:scale-105 cursor-pointer"
     >
       {type}
