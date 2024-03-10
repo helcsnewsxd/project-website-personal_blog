74261e17e9c4f85df26430491845e43cbbe297fe
helcsnewsxd <emanuelherrador2@gmail.com>
2024-03-10 17:08:23 -0300

    Best organization of about page and fixed and changed hero image

---
+++ components/about/skills.tsx
@@ -0,0 +1,10 @@
+export default function Skills() {
+  return (
+    <section className="pb-10 grid grid-flow-row grid-cols-1 space-x-5">
+      <div>
+        <h1 className="text-4xl text-balance font-bold pb-10">Habilidades</h1>
+        <div className="text-pretty text-left lg:text-lg text-base"></div>
+      </div>
+    </section>
+  );
+}
---
+++ components/about/who-am-i.tsx
@@ -0,0 +1,34 @@
+export default function WhoAmI() {
+  return (
+    <section className="pb-10">
+      <h1 className="text-4xl text-balance font-bold pb-10">¿Quién soy?</h1>
+      <div className="text-pretty text-left lg:text-lg text-base space-y-2">
+        <p>
+          Soy Emanuel Nicolás Herrador, un joven entusiasta que{" "}
+          <strong>ama</strong> la programación, los algoritmos, la tecnología y
+          la informática en general.
+        </p>
+        <p>
+          Desde chico me interesé por la <i>Matemática</i> y su relación con las
+          demás ciencias, y gracias al destino (y a mi hermana), me topé con
+          este maravilloso mundo. Inicié mis acercamientos con la programación
+          de la forma más <i>sutil</i> que conozco para aprender (sin rodeos y
+          con lo más complejo primero), con la Olimpíada Informática Argentina.
+          Así es, no conocía nada de programación pero me metí a la olimpíada
+          para aprender. Y vaya que aprendí.
+        </p>
+        <p>
+          Desde ese entonces, la programación me atrapó y lo tuve muy claro,
+          había encontrado mi vocación.
+        </p>
+        <p>
+          Me encanta aprender cosas nuevas, y me apasiona enseñar y compartir lo
+          que sé. Me gusta ayudar a los demás y siempre estoy dispuesto a
+          aprender de los demás. El conocimiento es la herramienta más poderosa
+          que tenemos, y es por eso que debemos compartirlo y apoyarnos
+          mutuamente para crecer.
+        </p>
+      </div>
+    </section>
+  );
+}
