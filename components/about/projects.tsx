cd71857b2fa8446a4b85d7e380a31c1684093d36
helcsnewsxd <emanuelherrador2@gmail.com>
2024-03-15 03:01:15 -0300

    Best projects. Idea of my sister

--- components/about/projects.tsx
+++ components/about/projects.tsx
@@ -7,21 +7,23 @@ type Props = {
   allProjects: ProjectType[];
 };
 
-const cntProjects = 3;
+const projectList = ["website", "assembly_animation", "process_scheduling"];
 
 export default function CoolProjects({ allProjects }: Props) {
   return (
     <section className="pb-10">
       <h1 className="text-4xl text-balance font-bold pb-10">
-        Últimos proyectos
+        Mejores proyectos
       </h1>
       <div className="flex flex-col items-center">
         <div className="grid grid-flow-row lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 mb-5">
-          {allProjects.slice(0, cntProjects).map((project) => (
-            <div key={project.slug}>
-              <ProjectCard project={project} />
-            </div>
-          ))}
+          {allProjects
+            .filter((project) => projectList.includes(project.slug))
+            .map((project) => (
+              <div key={project.slug}>
+                <ProjectCard project={project} />
+              </div>
+            ))}
         </div>
         <Link href="/projects">
           <button className="btn btn-primary hover:scale-105">Ver más</button>
